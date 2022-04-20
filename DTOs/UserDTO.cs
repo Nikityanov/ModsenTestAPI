3d0d11112942a6f0bdec9c4dc674688950760722
Nikityanov <dnikityanov@gmail.com>
2022-04-20 09:58:16 +0300

    Added some entities and DTOs to project

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ModsenTestAPI.DTOs
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Entities/Meetup.cs
@@ -0,0 +1,12 @@
+﻿namespace ModsenTestAPI.Entities
+{
+    public class Meetup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Place { get; set; } = string.Empty ;
+        public string[] Tags { get; set; }
+    }
+}
